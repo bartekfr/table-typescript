e9cbb9fdde6245784f39323c9688c8200a58e1c6
Bartek F <bartekfr@wp.pl>
2022-03-01 18:45:25 +0100

    Refactor and improve tests

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -7,39 +7,48 @@ describe('App test', () => {
   })
 
   it('Adds columns', () => {
-    let columnsLength = 3
+    let expectedColumnsLength = 3
     // Add new column
-    cy.get('tr:first-child th:first-child div:first-child').as('buttons1')
-    cy.get('@buttons1').find('button:last-child').click({ force: true })
-    columnsLength++
+    cy.get('tr:first-child th:first-child div:first-child button:last-child')
+      .click({ force: true })
 
-    cy.get('tr:first-child th').should('have.length', columnsLength)
+    expectedColumnsLength++
+
+    cy.get('tr:first-child th').should('have.length', expectedColumnsLength)
     cy.get('tr:first-child th:nth-child(2) input').should('have.value', 'New column 1')
 
     // Add new column to the right
     const baseColumnCssIndex = 3 // css indices are 1-based.
-    cy.get(`tr:first-child th:nth-child(${baseColumnCssIndex}) div:first-child`).as('buttons2')
-    cy.get('@buttons2').find('button:last-child').click({ force: true })
-    columnsLength++
+    cy.get(`tr:first-child th:nth-child(${baseColumnCssIndex}) div:first-child`).as('buttons')
+      .find('button:last-child')
+      .click({ force: true })
 
-    cy.get('tr:first-child th').should('have.length', columnsLength)
+    expectedColumnsLength++
+
+    cy.get('tr:first-child th').should('have.length', expectedColumnsLength)
     cy.get(`tr:first-child th:nth-child(${baseColumnCssIndex + 1}) input`).should('have.value', 'New column 3')
 
     // Add new column to the left
-    cy.get('@buttons2').find('button:first-child').click({ force: true })
-    columnsLength++
+    cy.get('@buttons').find('button:first-child').click({ force: true })
+    expectedColumnsLength++
 
-    cy.get('tr:first-child th').should('have.length', columnsLength)
+    cy.get('tr:first-child th').should('have.length', expectedColumnsLength)
     cy.get(`tr:first-child th:nth-child(${baseColumnCssIndex}) input`).should('have.value', 'New column 2')
   })
 
   it('Edits cells', () => {
-    cy.get('tr:nth-child(2) td:nth-child(2) input').type('xyz{enter}')
-    cy.get('tr:nth-child(2) td:nth-child(2) input').should('have.value', 'xyz')
+    cy.get('tr:nth-child(2) td:nth-child(2) input')
+      .should('have.value', '')
+      .type('xyz{enter}')
+      .should('have.value', 'xyz')
 
-    cy.get('tr:nth-child(3) td:nth-child(3) input').type('abc{enter}')
-    cy.get('tr:nth-child(3) td:nth-child(3) input').should('have.value', 'abc')
-
+    // just play with `within`
+    cy.get('tr:nth-child(3) td:nth-child(3) input').within(($cellInput) => {
+      cy.wrap($cellInput)
+        .should('have.value', '')
+        .type('abc{enter}')
+        .should('have.value', 'abc')
+    })
 
     cy.get('tr:nth-child(3) td:nth-child(4) input')
       .should('have.value', 'b1')
